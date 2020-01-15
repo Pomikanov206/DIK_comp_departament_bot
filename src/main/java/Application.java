f0b470c110c2704f0dded8192f50d1dcf7cd53e1
Pomik <pomikanov@gmail.com>
2020-01-16 01:09:59 +0200

    Add web host

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,7 +2,9 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 import view.Bot;
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
 public class Application {
     public static void main(String[] args) {
         ApiContextInitializer.init();
@@ -13,4 +15,15 @@ public class Application {
             e.printStackTrace();
         }
     }
+}*/
+
+
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
